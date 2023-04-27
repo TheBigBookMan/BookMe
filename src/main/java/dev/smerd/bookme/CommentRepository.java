e89f0a9b7ec372043ba0cc4ec8cf2682cbbc55dc
Ben Smerd <bsmerd@adam.com.au>
2023-04-27 21:04:01 +0930

    add in the comment repository which extends the mongo repository

---
+++ src/main/java/dev/smerd/bookme/CommentRepository.java
@@ -0,0 +1,12 @@
+package dev.smerd.bookme;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+// annotate it is a repositry
+// the extend of mongoRepsotiroy gets the findAll method which gets all the Objects back from the mongo
+// extending mongoRepository gives access to the mongo database commands like findAll, create etc
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, ObjectId> {
+}
