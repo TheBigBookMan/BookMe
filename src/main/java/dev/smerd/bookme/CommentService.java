0bf65ee5aafe4ef29af430f002b08268350ac907
Ben Smerd <bsmerd@adam.com.au>
2023-04-28 10:04:42 +0930

    add in comment service component with dependency injection from the mongoTemplate

---
+++ src/main/java/dev/smerd/bookme/CommentService.java
@@ -0,0 +1,29 @@
+package dev.smerd.bookme;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    // template is a more specific query to the database that requires more complex logic
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Comment createComment(String commentBody, ObjectId bookId) {
+        //   this is creating a new comment by using the reviewRepository mongo function of insert
+        Comment comment = commentRepository.insert(new Comment(commentBody));
+
+//        this is updating the book with the new comment id
+        mongoTemplate.update(Book.class)
+                .matching(Criteria.where("id").is(bookId))
+                .apply(new Update().push("commentIds").value(comment))
+                .first();
+        return comment;
+    }
+}
