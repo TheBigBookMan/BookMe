bc76d689bcd71ffffecedf52667f01b29047342a
Ben Smerd <bsmerd@adam.com.au>
2023-04-28 19:06:12 +0930

    fix bug for bookId when creating a comment

--- src/main/java/dev/smerd/bookme/Comment.java
+++ src/main/java/dev/smerd/bookme/Comment.java
@@ -16,7 +16,6 @@ public class Comment {
     private ObjectId id;
     private String commentBody;
     private int upvotes;
-    private String bookId;
 
     // custom constructor that takes just body because objectId auto generated and has @Allargsconstructor
     // and @NoArgsconstructor
