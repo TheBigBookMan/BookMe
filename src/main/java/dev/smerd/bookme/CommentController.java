16726fe510d22dbf1fbf9120010d0ca54f90eff4
Ben Smerd <bsmerd@adam.com.au>
2023-04-29 08:44:32 +0930

    add in upvote functionality to the comment service and comment controller

--- src/main/java/dev/smerd/bookme/CommentController.java
+++ src/main/java/dev/smerd/bookme/CommentController.java
@@ -1,12 +1,10 @@
 package dev.smerd.bookme;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -20,5 +18,10 @@ public class CommentController {
     public ResponseEntity<Comment> createComment(@RequestBody Map<String, String> payload) {
         //        the return will contain the reviewBody and bookId
         return new ResponseEntity<Comment>(commentService.createComment(payload.get("commentBody"), payload.get("bookId")), HttpStatus.CREATED);
+    }
+
+    @PostMapping("upvote/{commentId}")
+    public void upvoteComment(@PathVariable ObjectId commentId) {
+        commentService.upvote(commentId);
     }
 }
--- src/main/java/dev/smerd/bookme/CommentService.java
+++ src/main/java/dev/smerd/bookme/CommentService.java
@@ -1,5 +1,6 @@
 package dev.smerd.bookme;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -24,5 +25,14 @@ public class CommentService {
                 .apply(new Update().push("commentIds").value(comment))
                 .first();
         return comment;
+    }
+
+    public void upvote(ObjectId commentId) {
+//        update the comment by adding one onto the upvotes tally
+        mongoTemplate.update(Comment.class)
+                .matching(Criteria.where("id").is(commentId))
+                .apply(new Update().inc("upvotes", 1))
+                .first();
+//        return Comment;
     }
 }
