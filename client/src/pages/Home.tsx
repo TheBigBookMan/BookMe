b2b8ed48c6870ce9332c8c6d7a7106ad1f160451
Ben Smerd <bsmerd@adam.com.au>
2023-05-01 12:03:24 +0930

    add CORS config update to allow localhost client

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,23 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-    const [books, setBooks] = useState<
+    const [books, setBooks] = useState<Book[]>([]);
+
+    const getData: void = async () => {
+        try {
+            const response = await axios.get(
+                "http://localhost:8090/api/v1/books"
+            );
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    });
 
     return (
         <div>
