4a3b2791b6d3a651b501ad547449911e82795489
Ben Smerd <bsmerd@adam.com.au>
2023-05-02 13:03:52 +0930

    add in the singular book page

--- client/src/pages/Book.tsx
+++ client/src/pages/Book.tsx
@@ -1,28 +1,47 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../utils/axios";
+import { AiFillStar } from "react-icons/ai";
 
 const Book = () => {
     const { bookId } = useParams();
     const [bookData, setBookData] = useState<Book>();
 
-    const getBook = async (): Promise<void> => {
-        try {
-            const { data } = await api.get(`/books/${bookId}`);
-            setBookData({ ...data });
-        } catch (err) {
-            console.log(err);
-        }
+    const getBookData = async (): Promise<void> => {
+        const { data } = await api.get(`/books/${bookId}`);
+        setBookData(data);
     };
 
     useEffect(() => {
-        getBook();
-    }, []);
+        getBookData();
+    }, [bookId]);
 
     console.log(bookData);
     return (
-        <div>
-            <h1>book</h1>
+        <div className="p-4">
+            {!bookData ? (
+                <h1 className="font-bold text-2xl">Loading...</h1>
+            ) : (
+                <div className="flex flex-col items-center h-[500px]  p-2">
+                    <img src={bookData.image} className="w-32 rounded-lg" />
+                    <h1 className="font-bold text-2xl">{bookData.title}</h1>
+                    <p className="italic">
+                        {bookData.authors.map((author) => author + ", ")}
+                    </p>
+                    <p>
+                        {bookData.categories.map((category) => category + ", ")}
+                    </p>
+                    <div className="flex gap-2">
+                        <div className="flex gap-1 items-center justify-center">
+                            <AiFillStar className="text-yellow-500" />
+                            <p>{bookData.rating}</p>
+                        </div>
+                        <p>{bookData.publishedDate}</p>
+                    </div>
+                    <p>{bookData.subtitle}</p>
+                    <p className="overflow-y-auto">{bookData.description}</p>
+                </div>
+            )}
         </div>
     );
 };
