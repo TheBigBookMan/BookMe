6d7b18a95bfd3f3c15a07da91e661bbf585a5782
Ben Smerd <bsmerd@adam.com.au>
2023-05-02 12:12:45 +0930

    add in axios GET call to the single book API

--- client/src/pages/Book.tsx
+++ client/src/pages/Book.tsx
@@ -1,10 +1,25 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { api } from "../utils/axios";
 
 const Book = () => {
     const { bookId } = useParams();
-    console.log(bookId);
+    const [bookData, setBookData] = useState<Book>();
 
+    const getBook = async (): Promise<void> => {
+        try {
+            const { data } = await api.get(`/books/${bookId}`);
+            setBookData({ ...data });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getBook();
+    }, []);
+
+    console.log(bookData);
     return (
         <div>
             <h1>book</h1>
